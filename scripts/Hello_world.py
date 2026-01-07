cdc709ccdb10a044ebf37c6a09664baca93be8fd
jay sojitra <sojitrajay07@gmail.com>
2026-01-06 20:14:34 -0600

    Hello World python program

---
+++ scripts/Hello_world.py
@@ -0,0 +1,24 @@
+#We are wrieting the code insid the python using the QGIS python console!
+"""
+you can downldod the QGIS at their wabsie, Ima using the the version 3.40
+ ( longer vesion) i jsut write a print statmnet and tahn how you can wriet
+ a hellow word program.
+"""
+
+# Also you cna watch mthe refence video at youtube to work with that!
+# I am provid  video
+# link here: https://youtu.be/p60q4uiCqI0?list=PLppGmFLhQ1HKKnk3riKNyOxb-3MTI-7z
+
+# Also i forget you cna open the python consle insdi th qgis plugin-> python 
+# an dthen click show editer and you an work on it!
+
+
+print('Hello World')
+print('Hello class')
+
+print('This is the Hellow world program inside the qgis!')
+
+
+
+
+
